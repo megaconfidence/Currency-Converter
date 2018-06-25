1567d278392e32e04bb320e6427864a326e4d1b8
Confidence Okoghenun <confidenceboi@gmail.com>
2018-06-25 02:56:56 +0100

    Working Online Version
    This commit includes changes that implements
    the use of currency api while online.
    
    *An event listner was placed on convert
     button which triggers use of api while
     online and calls function 'showResult' to
     display results of calculation.
    *This has made the app fully functional.
    *Next up would be making the app a PWA.
    
    [Closes-Bug: #003]

--- js/main.js
+++ js/main.js
@@ -39,3 +39,28 @@ function hasProperty(val) {
     return "";
   }
 }
+
+document.querySelector('#convert').addEventListener('click', (e) => {
+    e.preventDefault();
+    let currency1 = document.querySelector('#from_currency').value;
+    let currency2 = document.querySelector('#to_currency').value;
+       fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${currency1}_${currency2}&compact=y`)
+    .then(res => res.json())
+    .then(resultJson => {
+      showResult(resultJson[`${currency1}_${currency2}`].val);
+    })
+    .catch(err => console.log(err));
+
+    function showResult(result) {
+        let calculateResult  = document.querySelector('#from_value').value*result;
+       let stringedResult = calculateResult.toString();
+       if(stringedResult.includes('.')){
+           let resultArr = stringedResult.split('.');
+           let part2 = `${resultArr[1][0]}${hasProperty(resultArr[1][1])}`
+           let finalResult = Number(`${resultArr[0]}.${part2}`);
+           document.querySelector('#result').value = finalResult;
+       } else {
+        document.querySelector('#result').value = calculateResult;
+       }
+    }
+})
