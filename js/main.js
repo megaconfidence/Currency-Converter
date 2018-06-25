068b787b1cf464360df4c490bd78041ea023bb43
Confidence Okoghenun <confidenceboi@gmail.com>
2018-06-25 13:25:11 +0100

    IndexedDB Implementation
    In this commit, the use of indexedDB was
    implemented to store currency conversion
    values gotten form the api and render results
    from already stored values.
    
    *Localforge framework was used to set and
     get values form the indexedDB.
    *This has made the app able to work
     offline if the currency has been previously
     converted when online.
    *Next up would be integration of service
     workers.
    
    [Closes-Bug: #004]

--- js/main.js
+++ js/main.js
@@ -40,27 +40,67 @@ function hasProperty(val) {
   }
 }
 
-document.querySelector('#convert').addEventListener('click', (e) => {
-    e.preventDefault();
-    let currency1 = document.querySelector('#from_currency').value;
-    let currency2 = document.querySelector('#to_currency').value;
-       fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${currency1}_${currency2}&compact=y`)
-    .then(res => res.json())
-    .then(resultJson => {
-      showResult(resultJson[`${currency1}_${currency2}`].val);
+document.querySelector("#convert").addEventListener("click", e => {
+  e.preventDefault();
+  let currency1 = document.querySelector("#from_currency").value;
+  let currency2 = document.querySelector("#to_currency").value;
+  //checks if item is in indexedDB and makes a decision based on outcome
+  localforage
+    .getItem(`${currency1}_${currency2}`)
+    .then(value => {
+      if (value) {
+        console.log("Getting from DB...");
+        getFromIndexedDB(value);
+      } else {
+        console.log("Getting from net...");
+        getFromNet();
+      }
     })
     .catch(err => console.log(err));
 
-    function showResult(result) {
-        let calculateResult  = document.querySelector('#from_value').value*result;
-       let stringedResult = calculateResult.toString();
-       if(stringedResult.includes('.')){
-           let resultArr = stringedResult.split('.');
-           let part2 = `${resultArr[1][0]}${hasProperty(resultArr[1][1])}`
-           let finalResult = Number(`${resultArr[0]}.${part2}`);
-           document.querySelector('#result').value = finalResult;
-       } else {
-        document.querySelector('#result').value = calculateResult;
-       }
+  //gets value from indexedDB and renders it
+  function getFromIndexedDB(gottenValue) {
+    let calculateResult =
+      document.querySelector("#from_value").value * gottenValue;
+    let stringedResult = calculateResult.toString();
+    if (stringedResult.includes(".")) {
+      let resultArr = stringedResult.split(".");
+      let part2 = `${resultArr[1][0]}${hasProperty(resultArr[1][1])}`;
+      let finalResult = Number(`${resultArr[0]}.${part2}`);
+      document.querySelector("#result").value = finalResult;
+    } else {
+      document.querySelector("#result").value = calculateResult;
     }
-})
+  }
+  //gets value from net api saves and renders it
+  function getFromNet() {
+    fetch(
+      `https://free.currencyconverterapi.com/api/v5/convert?q=${currency1}_${currency2}&compact=y`
+    )
+      .then(res => res.json())
+      .then(resultJson => {
+        showResult(resultJson[`${currency1}_${currency2}`].val);
+      })
+      .catch(err => console.log(err));
+
+    function showResult(result) {
+      //save result to indexedDB
+      localforage
+        .setItem(`${currency1}_${currency2}`, result)
+        .then(value => console.log(`Saved ${value} to DB`))
+        .catch(err => console.log(err));
+
+      let calculateResult =
+        document.querySelector("#from_value").value * result;
+      let stringedResult = calculateResult.toString();
+      if (stringedResult.includes(".")) {
+        let resultArr = stringedResult.split(".");
+        let part2 = `${resultArr[1][0]}${hasProperty(resultArr[1][1])}`;
+        let finalResult = Number(`${resultArr[0]}.${part2}`);
+        document.querySelector("#result").value = finalResult;
+      } else {
+        document.querySelector("#result").value = calculateResult;
+      }
+    }
+  }
+});
