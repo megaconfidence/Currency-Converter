ff32fec31c963c46483a6399d2443ba92323894a
Confidence Okoghenun <confidenceboi@gmail.com>
2018-06-27 00:14:16 +0100

    Bug Fixes

--- js/main.js
+++ js/main.js
@@ -154,3 +154,24 @@ function initSelectForm(options) {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, options);
 }
+//install PWA
+let installPromptEvent;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installPromptEvent = event;
+  document.querySelector('#install-button').disabled = false;
+});
+
+btnInstall.addEventListener('click', () => {
+  document.querySelector('#install-button').disabled = true;
+  installPromptEvent.prompt();
+  installPromptEvent.userChoice.then((choice) => {
+    if (choice.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+    installPromptEvent = null;
+  });
+});
