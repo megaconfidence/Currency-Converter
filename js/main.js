7b6287296a7f7198ea508a7365a5536251ffe0f8
Confidence Okoghenun <confidenceboi@gmail.com>
2018-06-28 08:47:17 +0100

    Major Performance Improvements

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register('/Currency-Converter/sw.js', {scope: '/Currency-Converter/'})
-    // .register('../sw.js')
+    // .register("../sw.js")
     .then(() => console.log("Service worker registered"));
 }
 
@@ -11,21 +11,21 @@ if ("caches" in window) {
     .then(res => {
       if (res) {
         res.json().then(json => {
-          console.log('Fetching form cache...')
-          bulidOptions(json.results)
+          console.log("Fetching form cache...");
+          bulidOptions(json.results);
         });
       } else {
         fetchFromApi();
       }
     });
 } else {
-  console.log('Fetching form API...')
+  console.log("Fetching form API...");
   fetchFromApi();
 }
 
 function fetchFromApi() {
   // fetch("../currencies.json")
-  fetch("https://free.currencyconverterapi.com/api/v5/currencies")
+    fetch("https://free.currencyconverterapi.com/api/v5/currencies")
     .then(res => res.json())
     .then(currencyJson => {
       bulidOptions(currencyJson.results);
@@ -34,33 +34,42 @@ function fetchFromApi() {
 }
 
 function bulidOptions(currencies) {
-  selectFrom(currencies);
-  selectTo(currencies);
+  bulidCurrencyArr(currencies);
 }
-function selectFrom(currencies) {
+
+let currencyArr = [];
+
+function bulidCurrencyArr(currencies) {
   for (let currency in currencies) {
     if (currencies.hasOwnProperty(currency)) {
       let currName = currencies[currency].currencyName;
-      let currOption = document.createElement("option");
-      currOption.setAttribute("value", currency);
-      currOption.textContent = `${currName} (${currency})`;
-      document.querySelector("#from_currency").appendChild(currOption);
+      currencyArr.push(`${currName} (${currency})`);
     }
   }
-  let options = document.querySelectorAll('option');
+  selectFrom();
+  selectTo();
+}
+function selectFrom() {
+  sortedArr = currencyArr.sort();
+  sortedArr.forEach(currencyItem => {
+    let currOption = document.createElement("option");
+    currOption.setAttribute("value",currencyItem.slice(currencyItem.length - 4, currencyItem.length - 1));
+    currOption.textContent = currencyItem;
+    document.querySelector("#from_currency").appendChild(currOption);
+  });
+  let options = document.querySelectorAll("option");
   initSelectForm(options);
 }
-function selectTo(currencies) {
-  for (let currency in currencies) {
-    if (currencies.hasOwnProperty(currency)) {
-      let currName = currencies[currency].currencyName;
-      let currOption = document.createElement("option");
-      currOption.setAttribute("value", currency);
-      currOption.textContent = `${currName} (${currency})`;
-      document.querySelector("#to_currency").appendChild(currOption);
-    }
-  }
-  let options = document.querySelectorAll('option');
+
+function selectTo() {
+  sortedArr = currencyArr.sort();
+  sortedArr.forEach(currencyItem => {
+    let currOption = document.createElement("option");
+    currOption.setAttribute("value", currencyItem.slice(currencyItem.length - 4, currencyItem.length - 1));
+    currOption.textContent = currencyItem;
+    document.querySelector("#to_currency").appendChild(currOption);
+  });
+  let options = document.querySelectorAll("option");
   initSelectForm(options);
 }
 
@@ -74,22 +83,26 @@ function hasProperty(val) {
 
 document.querySelector("#convert").addEventListener("click", e => {
   e.preventDefault();
-  document.querySelector("#convert").innerHTML = 'Converting<i id="loop-icon" class="material-icons right">autorenew</i>';
-  document.querySelector("#result").style.display = 'none';
-  let loader = document.querySelectorAll('.progress');
-  loader.forEach(ele => ele.style.display = 'block');
-  setTimeout(() => {
-    loader.forEach(ele => ele.style.display = 'none');
-    document.querySelector("#convert").innerHTML = 'Convert<i id="loop" class="material-icons right">autorenew</i>';
-    document.querySelector("#result").style.display = 'block';
 
-  },3000)
   let currency1 = document.querySelector("#from_currency").value;
   let currency2 = document.querySelector("#to_currency").value;
 
+  if((currency1 !== '')&&(currency2 !== '')) {
 
-  //checks if item is in indexedDB and makes a decision based on outcome
-  localforage
+    document.querySelector("#convert").innerHTML =
+    'Converting<i id="loop-icon" class="material-icons right">autorenew</i>';
+  document.querySelector("#result").style.display = "none";
+  let loader = document.querySelectorAll(".progress");
+  loader.forEach(ele => (ele.style.display = "block"));
+  setTimeout(() => {
+    loader.forEach(ele => (ele.style.display = "none"));
+    document.querySelector("#convert").innerHTML =
+      'Convert<i id="loop" class="material-icons right">autorenew</i>';
+    document.querySelector("#result").style.display = "block";
+  }, 3000);
+
+    //checks if item is in indexedDB and makes a decision based on outcome
+    localforage
     .getItem(`${currency1}_${currency2}`)
     .then(value => {
       if (value) {
@@ -107,39 +120,120 @@ document.querySelector("#convert").addEventListener("click", e => {
             //save result to indexedDB
             let value = resultJson[`${currency1}_${currency2}`].val;
             localforage
-            .setItem(`${currency1}_${currency2}`, value)
-            .then(value => console.log(`Saved ${value} to DB`))
-            .catch(err => console.log(err));
-    
+              .setItem(`${currency1}_${currency2}`, value)
+              .then(value => console.log(`Saved ${value} to DB`))
+              .catch(err => console.log(err));
+
             showResult(value);
           })
-          .catch(err => console.log(err));
+          .catch(err => {
+            M.toast({html: 'You need the internet for every initial currency conversion!'})
+            console.log(err)
+          });
       }
     })
     .catch(err => console.log(err));
+  } else {
+    M.toast({html: 'Please enter valid inputs'})
+  }
 
   function showResult(result) {
-    let currency = document.querySelector("#to_currency").value
-    
-    let calculateResult =
-      document.querySelector("#from_value").value * result;
+    let currency = document.querySelector("#to_currency").value;
+    let amount = (document.querySelector("#from_value").value || 1);
+    let calculateResult = amount * result;
+    calculateResult = toFixed(calculateResult);
     let stringedResult = calculateResult.toString();
+    let hysResult;
     if (stringedResult.includes(".")) {
       let resultArr = stringedResult.split(".");
       let part2 = `${resultArr[1][0]}${hasProperty(resultArr[1][1])}`;
-      let finalResult = (`${resultArr[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}.${part2}`);
+      let finalResult = `${resultArr[0]
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}.${part2}`;
+      hysResult = finalResult;
       document.querySelector("#result").value = `${currency} ${finalResult}`;
     } else {
-      document.querySelector("#result").value = `${currency} ${calculateResult.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+      hysResult = `${calculateResult
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+      document.querySelector(
+        "#result"
+      ).value = `${currency} ${calculateResult
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
     }
+    //save to history
+    let toCurrency = currency;
+    let fromCurrency = document.querySelector("#from_currency").value;
+    let exchangeRate = result;
+    history(amount, fromCurrency, toCurrency, hysResult, exchangeRate);
+  }
+
+  function toFixed(x) {
+    if (Math.abs(x) < 1.0) {
+      var e = parseInt(x.toString().split("e-")[1]);
+      if (e) {
+        x *= Math.pow(10, e - 1);
+        x = "0." + new Array(e).join("0") + x.toString().substring(2);
+      }
+    } else {
+      var e = parseInt(x.toString().split("+")[1]);
+      if (e > 20) {
+        e -= 20;
+        x /= Math.pow(10, e);
+        x += new Array(e + 1).join("0");
+      }
+    }
+    return x;
+  }
+
+  function history(amount, fromCurrency, toCurrency, result, exchangeRate) {
+    if (historyArr.length === 5) {
+      historyArr.shift();
+    }
+    historyArr.push(
+      `${amount} ${fromCurrency} > ${toCurrency} = ${toCurrency} ${result} (Exchange rate : 1 ${fromCurrency} = ${exchangeRate} ${toCurrency})`
+    );
+    localforage
+      .setItem("History", historyArr)
+      .then(value => {
+        console.log("Updated history with =>");
+        console.log(value);
+      })
+      .catch(err => console.log(err));
+    console.log(historyArr);
   }
 });
 
+let historyArr = [];
 
+document.querySelector("#history-btn").addEventListener("click", () => {
+  document.querySelector("#calculate-card").style.display = "none";
+  document.querySelector("#history-card").style.display = "block";
+
+  let historyOl = document.querySelector("#history-ol");
+  historyOl.textContent = "";
+  localforage
+    .getItem("History")
+    .then(value => {
+      value.forEach((hysItem, i) => {
+        let paragraph = document.createElement("li");
+        paragraph.textContent = hysItem;
+        historyOl.insertAdjacentElement("beforeend", paragraph);
+      });
+      console.log(value);
+    })
+    .catch(err => console.log(err));
+});
+
+document.querySelector("#history-close").addEventListener("click", () => {
+  document.querySelector("#calculate-card").style.display = "block";
+  document.querySelector("#history-card").style.display = "none";
+});
 
 //materialize css plugins
 //plugin for select
 function initSelectForm(options) {
-  var elems = document.querySelectorAll('select');
+  var elems = document.querySelectorAll("select");
   var instances = M.FormSelect.init(elems, options);
 }
